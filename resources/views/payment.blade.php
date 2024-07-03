be2630b956960a3dfcc33bec9a1600d42c378a01
Dor Katzir <katzirdor@gmail.com>
2024-07-03 23:15:06 +0300

    payment page logic

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -4,15 +4,16 @@
 
     <section class="container mt-2 my-3 py-5">
         <div class="container text-center mt-2">
-            <h4>Payment</h4>
+            <h4 class="my-5">Payment</h4>
             
-            @if(Session::has('total') && Session::get('totla') != null)
+            @if(Session::has('total') && Session::get('total') != null)
                 @if (Session::has('order_id') && Session::get('order_id') != null)
                     <h4 class="my-5" style="color:blue">
                         Total: ${{ Session::get('total') }}
                     </h4>
                 @endif
             @endif
+
             
         </div>
     </section>
